8b0905dedd37cdc8d25bc51d28de90dee579997a
Casey <caseychandlerh@yahoo.com>
2024-12-04 22:27:02 +0000

    Create 00-start.py

---
+++ .ipython/profile_default/00-start.py
@@ -0,0 +1,34 @@
+import itertools
+import json
+import os
+import re
+import sys
+
+import numpy as np
+import polars as pl
+
+from collections import Counter, defaultdict, namedtuple
+from pathlib import Path
+
+
+def imports():
+    for _, val in globals().items():
+        if isinstance(val, types.ModuleType):
+            yield val.__name__
+
+
+print(
+    "successfully imported: [{:s}]".format(
+        ", ".join(
+            sorted(
+                set(
+                    [
+                        '"{:s}"'.format(e)
+                        for e in imports()
+                        if "__" not in e and "types" not in e
+                    ]
+                )
+            )
+        )
+    )
+)
